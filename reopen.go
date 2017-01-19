984e316bfe3cb90736ad9c44b4cf510b3c3d8ad8
Dan McCombs <dmccombs@dyn.com>
2017-01-19 13:24:46 -0500

    Revert "Add dummy Flush() function."
    This reverts commit ad47975af69457b732ea6a96867a8fa2b677ac83.

--- reopen.go
+++ reopen.go
@@ -17,7 +17,6 @@ type Reopener interface {
 // Writer is a writer that also can be reopened
 type Writer interface {
 	Reopener
-	Flush()
 	io.Writer
 }
 
@@ -200,11 +199,6 @@ func (t *multiReopenWriter) Write(p []byte) (int, error) {
 		}
 	}
 	return len(p), nil
-}
-
-// Flush implements bufio.Writer
-func (t *multiReopenWriter) Flush() {
-	return
 }
 
 // MultiWriter creates a writer that duplicates its writes to all the
